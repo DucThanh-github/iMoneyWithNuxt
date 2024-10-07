6bf9ef5e225781bafde4e39901e2f2bae409b31a
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2024-10-07 23:23:44 +0700

    c1 branch 2

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
     <v-row class="text-red-300">
         Home page
         <v-col>Branch 1</v-col>
+        <span>commit 1 branch 2</span>
     </v-row>
+    
 </template>
 
 <script setup lang="ts">
