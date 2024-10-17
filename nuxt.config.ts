e3102378a2249dcf87c5718e6fdbce1e1fc19040
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2024-10-17 22:41:39 +0700

    Revert "fix runtime config nuxt config"
    This reverts commit c1a7f5470ff45701c049aa986df7cb6475998286.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,12 +32,12 @@ export default defineNuxtConfig({
   plugins: ["~/plugins/firebase.client.ts"],
   runtimeConfig: {
     public: {
-      API_KEY: process.env.API_KEY,
-      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-      PROJECT_ID: process.env.PROJECT_ID,
-      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-      APP_ID: process.env.APP_ID,
+      API_KEY: process.env.FIREBASE_API_KEY,
+      AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      APP_ID: process.env.FIREBASE_APP_ID,
     },
   },
 });
