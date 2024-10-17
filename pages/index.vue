305e433d6e7f366590342f4fffff6d8ca796392e
Thanh_Nguyen <nguyenducthanh10121995@gmail.com>
2024-10-17 22:50:09 +0700

    Revert "c1 of branch 1"
    This reverts commit 4a73c14f604b9a363c2330b3feaf587383f9d1de.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
     <v-row class="text-red-300">
         Home page
-        <v-col>Branch 1</v-col>
-        <span>commit 1 branch 2</span>
-        <span>Commit 2 branch 1</span>
-        <span>Commit 3 branch 1</span>
-        <span>commit 2 branch 2</span>
     </v-row>
     
 </template>
